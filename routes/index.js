d36f518e81dba94855ff8e40f0711656e836bcf7
zhangmingrui <592044573@qq.com>
2017-06-01 15:08:30 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,17 @@ router.get('/demo/:id?', function(req, res, next) {
 router.get('/layer', function(req, res, next) {
     res.render('layer', { title: '系统弹层使用'});
 });
+/* io fetch 页 */
+router.get('/iofetch', function(req, res, next) {
+    res.render('iofetch', { title: 'node-io-fetch组件使用'});
+});
+/* io 页 */
+router.get('/io', function(req, res, next) {
+    res.render('io', { title: 'io接口调用'});
+});
+/* storage 页 */
+router.get('/storage', function(req, res, next) {
+    res.render('storage', { title: 'storage存储'});
+});
 
 module.exports = router;
